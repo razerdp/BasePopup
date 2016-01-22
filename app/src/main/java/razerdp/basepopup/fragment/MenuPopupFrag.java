206ba74d8839323eaec6cc3e23f4a272db00b9bc
430public <164701463@qq.com>
2016-01-22 17:59:36 +0800

    // add menu_popup demo & fix basepopupwindow bug

---
+++ app/src/main/java/razerdp/basepopup/fragment/MenuPopupFrag.java
@@ -0,0 +1,42 @@
+package razerdp.basepopup.fragment;
+
+import android.view.View;
+import android.widget.Button;
+import razerdp.basepopup.R;
+import razerdp.basepopup.base.BasePopupWindow;
+import razerdp.basepopup.popup.MenuPopup;
+
+/**
+ * Created by 大灯泡 on 2016/1/22.
+ * menu
+ */
+public class MenuPopupFrag extends SimpleBaseFrag{
+    private Button mButton;
+    private MenuPopup mMenuPopup;
+    @Override
+    public void bindEvent() {
+        mButton= (Button) mFragment.findViewById(R.id.popup_show);
+        mMenuPopup=new MenuPopup(mContext);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMenuPopup.showPopupWindow(v);
+            }
+        });
+    }
+
+    @Override
+    public BasePopupWindow getPopup() {
+        return null;
+    }
+
+    @Override
+    public Button getButton() {
+        return null;
+    }
+
+    @Override
+    public View getFragment() {
+        return mInflater.inflate(R.layout.frag_menu_popup,container,false);
+    }
+}
