e119e7a00424c26f32f4dd147445c23230aef638
razerdp <razerdp123@gmail.com>
2016-01-23 18:02:09 +0800

    // change contributor

---
+++ app/src/main/java/razerdp/basepopup/fragment/DialogPopupFrag.java
@@ -0,0 +1,45 @@
+package razerdp.basepopup.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import razerdp.basepopup.R;
+import razerdp.basepopup.base.BasePopupWindow;
+import razerdp.basepopup.popup.DialogPopup;
+import razerdp.basepopup.popup.SlideFromBottomPopup;
+
+/**
+ * Created by 大灯泡 on 2016/1/15.
+ * 客串dialog
+ */
+public class DialogPopupFrag extends SimpleBaseFrag {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void bindEvent() {
+
+    }
+
+    @Override
+    public BasePopupWindow getPopup() {
+        return new DialogPopup(mContext);
+    }
+
+    @Override
+    public Button getButton() {
+        return (Button) mFragment.findViewById(R.id.popup_show);
+    }
+
+    @Override
+    public View getFragment() {
+        return mInflater.inflate(R.layout.frag_dialog_popup,container,false);
+    }
+}
