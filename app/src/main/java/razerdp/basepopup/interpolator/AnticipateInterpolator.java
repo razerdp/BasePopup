357182de6d1fbc2a9bba10a854c96e037dd4ee67
razerdp <razerdp123@gmail.com>
2016-01-28 15:50:25 +0800

    // add interpolator popup && update gradle to alpha 7

---
+++ app/src/main/java/razerdp/basepopup/interpolator/AnticipateInterpolator.java
@@ -0,0 +1,24 @@
+package razerdp.basepopup.interpolator;
+
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by 大灯泡 on 2016/1/28.
+ * The expression comes from web:
+ * http://inloop.github.io/
+ */
+public class AnticipateInterpolator extends LinearInterpolator {
+    private float factor;
+
+    public AnticipateInterpolator() {
+        this.factor = 2.0f;
+    }
+    public AnticipateInterpolator(float factor) {
+        this.factor = factor;
+    }
+
+    @Override
+    public float getInterpolation(float input) {
+        return (float) input * input * ((factor + 1) * input - factor);
+    }
+}
---
+++ app/src/main/java/razerdp/basepopup/interpolator/OverShootInterpolator.java
@@ -0,0 +1,27 @@
+package razerdp.basepopup.interpolator;
+
+import android.view.animation.LinearInterpolator;
+
+/**
+ * Created by 大灯泡 on 2016/1/28.
+ * The expression comes from web:
+ * http://inloop.github.io/
+ */
+public class OverShootInterpolator extends LinearInterpolator {
+    private float factor;
+
+    public OverShootInterpolator() {
+        this.factor = 2.0f;
+    }
+
+    public OverShootInterpolator(float factor) {
+        this.factor = factor;
+    }
+
+    @Override
+    public float getInterpolation(float input) {
+
+        input -= 1.0;
+        return (float) (input * input * ((factor + 1) * input + factor) + 1.0);
+    }
+}
