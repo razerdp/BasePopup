1c9ef3277ff9596901bfd3ce101b774038d2c88b
razerdp <razerdp123@gmail.com>
2017-02-17 17:57:01 +0800

    // 修复androidN的showAsDropDown不能保证在anchor下面的问题

--- lib/src/main/java/razerdp/basepopup/PopupWindowProxy.java
+++ lib/src/main/java/razerdp/basepopup/PopupWindowProxy.java
@@ -1,17 +1,19 @@
 package razerdp.basepopup;
 
 import android.content.Context;
+import android.os.Build;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.PopupWindow;
 
 /**
  * Created by 大灯泡 on 2017/1/12.
- *
+ * <p>
  * 与basePopupWindow强引用(或者说与PopupController强引用)
  */
 
 public class PopupWindowProxy extends PopupWindow {
+    private final boolean isOverAndroidN = Build.VERSION.SDK_INT >= 24;
 
 
     private PopupController mController;
@@ -58,6 +60,29 @@ public class PopupWindowProxy extends PopupWindow {
     public PopupWindowProxy(View contentView, int width, int height, boolean focusable, PopupController mController) {
         super(contentView, width, height, focusable);
         this.mController = mController;
+    }
+
+
+    /**
+     * fix showAsDropDown when android api ver is over N
+     *
+     * https://code.google.com/p/android/issues/detail?id=221001
+     *
+     *
+     * @param anchor
+     * @param xoff
+     * @param yoff
+     * @param gravity
+     */
+    @Override
+    public void showAsDropDown(View anchor, int xoff, int yoff, int gravity) {
+        if (isOverAndroidN && anchor != null) {
+            int[] location = new int[2];
+            anchor.getLocationInWindow(location);
+            super.showAtLocation(anchor, gravity, xoff, location[1] + anchor.getHeight() + yoff);
+        } else {
+            super.showAsDropDown(anchor, xoff, yoff, gravity);
+        }
     }
 
     @Override
