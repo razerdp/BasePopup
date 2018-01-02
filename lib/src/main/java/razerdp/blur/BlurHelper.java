dafb17479392b6ce0a9d6727bde9182f984358b4
razerdp <razerdp123@gmail.com>
2018-01-02 11:15:32 +0800

    // 状态栏颜色

--- lib/src/main/java/razerdp/blur/BlurHelper.java
+++ lib/src/main/java/razerdp/blur/BlurHelper.java
@@ -1,10 +1,13 @@
 package razerdp.blur;
 
 import android.annotation.TargetApi;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.os.Build;
 import android.renderscript.Allocation;
 import android.renderscript.Element;
@@ -23,12 +26,18 @@ import razerdp.util.log.LogUtil;
  */
 class BlurHelper {
 
+    private static int statusBarHeight = 0;
+
     public static boolean renderScriptSupported() {
         return Build.VERSION.SDK_INT > Build.VERSION_CODES.JELLY_BEAN_MR1;
     }
 
     public static Bitmap blur(Context context, View view, float scaledRatio, float radius) {
-        return blur(context, getViewBitmap(view), scaledRatio, radius);
+        return blur(context, view, scaledRatio, radius, true);
+    }
+
+    public static Bitmap blur(Context context, View view, float scaledRatio, float radius, boolean fullScreen) {
+        return blur(context, getViewBitmap(view, fullScreen), scaledRatio, radius);
     }
 
     public static Bitmap blur(Context context, Bitmap origin, float scaledRatio, float radius) {
@@ -62,7 +71,7 @@ class BlurHelper {
         Bitmap scaledBitmap = Bitmap.createScaledBitmap(origin, scaledWidth, scaledHeight, false);
         Bitmap result = Bitmap.createBitmap(scaledBitmap);
 
-        if (scaledBitmap == null || scaledBitmap.isRecycled() || result == null || result.isRecycled()) {
+        if (scaledBitmap.isRecycled() || result == null || result.isRecycled()) {
             return null;
         }
 
@@ -131,14 +140,24 @@ class BlurHelper {
         return result;
     }
 
-    private static Bitmap getViewBitmap(final View v) {
+    private static Bitmap getViewBitmap(final View v, boolean fullScreen) {
         if (v == null || v.getWidth() <= 0 || v.getHeight() <= 0) {
             LogUtil.trace(LogTag.e, "getViewBitmap  >>  宽或者高为空");
             return null;
         }
+        if (statusBarHeight <= 0) statusBarHeight = getStatusBarHeight(v.getContext());
         Bitmap b = Bitmap.createBitmap(v.getWidth(), v.getHeight(), Bitmap.Config.ARGB_8888);
         Canvas c = new Canvas(b);
         c.drawColor(Color.WHITE);
+        if (fullScreen && statusBarHeight > 0 && Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if (v.getContext() instanceof Activity) {
+                int statusBarColor = ((Activity) v.getContext()).getWindow().getStatusBarColor();
+                Paint p = new Paint(Paint.ANTI_ALIAS_FLAG);
+                p.setColor(statusBarColor);
+                Rect rect = new Rect(0, 0, v.getWidth(), statusBarHeight);
+                c.drawRect(rect, p);
+            }
+        }
         v.draw(c);
         return b;
     }
@@ -150,5 +169,16 @@ class BlurHelper {
             originValue = rangeTo;
         }
         return originValue;
+    }
+
+
+    private static int getStatusBarHeight(Context context) {
+        if (context == null) return 0;
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
     }
 }
