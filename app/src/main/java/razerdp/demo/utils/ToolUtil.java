2387bda7c633e1e53adfec86ace6de210830adce
razerdp <razerdp123@gmail.com>
2018-12-09 01:57:46 +0800

    // fitsystembar

--- app/src/main/java/razerdp/demo/utils/ToolUtil.java
+++ app/src/main/java/razerdp/demo/utils/ToolUtil.java
@@ -1,5 +1,9 @@
 package razerdp.demo.utils;
 
+import android.content.Context;
+import android.content.res.Resources;
+
+import java.lang.reflect.Method;
 import java.util.List;
 
 /**
@@ -11,5 +15,26 @@ import java.util.List;
 public class ToolUtil {
     public static boolean isListEmpty(List<?> datas) {
         return datas == null || datas.size() <= 0;
+    }
+
+    public static boolean checkDeviceHasNavigationBar(Context context) {
+        boolean hasNavigationBar = false;
+        Resources rs = context.getResources();
+        int id = rs.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (id > 0) {
+            hasNavigationBar = rs.getBoolean(id);
+        }
+        try {
+            Class systemPropertiesClass = Class.forName("android.os.SystemProperties");
+            Method m = systemPropertiesClass.getMethod("get", String.class);
+            String navBarOverride = (String) m.invoke(systemPropertiesClass, "qemu.hw.mainkeys");
+            if ("1".equals(navBarOverride)) {
+                hasNavigationBar = false;
+            } else if ("0".equals(navBarOverride)) {
+                hasNavigationBar = true;
+            }
+        } catch (Exception e) {
+        }
+        return hasNavigationBar;
     }
 }
