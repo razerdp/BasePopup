2dd79059b6c06e5a6001bf91e8bf4a01116559c5
razerdp <razerdp123@gmail.com>
2018-12-18 14:26:40 +0800

    // full demo

--- app/src/main/java/razerdp/demo/DemoFullScreenActivity.java
+++ app/src/main/java/razerdp/demo/DemoFullScreenActivity.java
@@ -1,12 +1,17 @@
 package razerdp.demo;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.PopupWindow;
 
 import razerdp.basepopup.QuickPopupBuilder;
 import razerdp.basepopup.QuickPopupConfig;
@@ -20,16 +25,13 @@ public class DemoFullScreenActivity extends AppCompatActivity implements View.On
 
     private Button finish;
     private Button show;
+    private Button show2;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        Window window = getWindow();
-        WindowManager.LayoutParams params = window.getAttributes();
-        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        params.systemUiVisibility = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_IMMERSIVE;
-        window.setAttributes(params);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_full_screen);
         initView();
     }
@@ -40,6 +42,8 @@ public class DemoFullScreenActivity extends AppCompatActivity implements View.On
 
         finish.setOnClickListener(this);
         show.setOnClickListener(this);
+        show2 = (Button) findViewById(R.id.show2);
+        show2.setOnClickListener(this);
     }
 
     @Override
@@ -50,6 +54,9 @@ public class DemoFullScreenActivity extends AppCompatActivity implements View.On
                 break;
             case R.id.show:
                 showPopup();
+                break;
+            case R.id.show2:
+                showPopupBySystem();
                 break;
         }
     }
@@ -62,5 +69,30 @@ public class DemoFullScreenActivity extends AppCompatActivity implements View.On
                         .withShowAnimation(SimpleAnimationUtils.getDefaultScaleAnimation(true))
                         .withDismissAnimation(SimpleAnimationUtils.getDefaultScaleAnimation(false)))
                 .show();
+    }
+
+    private void showPopupBySystem() {
+        PopupWindow window = new PopupWindow(View.inflate(this, R.layout.popup_scale, null), ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT, true);
+        window.setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        window.setOutsideTouchable(true);
+        window.setTouchable(true);
+        window.showAtLocation(getWindow().getDecorView(), Gravity.CENTER, 0, 0);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+
+        // Standard Android full-screen functionality.
+        getWindow()
+                .getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
     }
 }
