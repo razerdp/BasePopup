e739590ec1ab79b93f78a0533711fd7eb9c8839a
razerdp <razerdp123@gmail.com>
2018-12-18 11:31:34 +0800

    // 2.1.2-beta

--- lib/src/main/java/razerdp/basepopup/PopupCompatManager.java
+++ lib/src/main/java/razerdp/basepopup/PopupCompatManager.java
@@ -86,11 +86,11 @@ final class PopupCompatManager {
 
 
         protected void onBeforeShowExec(BasePopupWindowProxy popupWindowProxy, Activity act) {
-
+//            fitSystemBar(act);
         }
 
         protected void onAfterShowExec(BasePopupWindowProxy popupWindowProxy, Activity act) {
-//            fitSystemBar(act, popupWindowProxy.getContentView());
+
         }
 
         boolean isPopupShowing(BasePopupWindowProxy popupWindow) {
@@ -101,14 +101,14 @@ final class PopupCompatManager {
          * 修复popup显示时退出沉浸的问题
          * 如果activity是全屏的，则popupwindow的window也应该
          */
-        void fitSystemBar(Activity act, View popupContentView) {
+        void fitSystemBar(Activity act) {
             if (act == null) return;
             try {
                 WindowManager.LayoutParams lp = act.getWindow().getAttributes();
                 int i = lp.flags;
                 if ((i & WindowManager.LayoutParams.FLAG_FULLSCREEN) != 0) {
                     //full screen
-                    hideSystemUI(act, popupContentView);
+                    hideSystemUI(act);
                 } else {
                     //showSystemUI(view);
                 }
@@ -117,24 +117,19 @@ final class PopupCompatManager {
             }
         }
 
-        private void hideSystemUI(Activity act, View popupContentView) {
-            if (popupContentView == null) return;
+        private void hideSystemUI(final Activity act) {
             try {
 
-                int uiOptions = act.getWindow().getDecorView().getSystemUiVisibility();
-                int newUiOptions = uiOptions;
-
-                if (Build.VERSION.SDK_INT >= 14) {
-                    newUiOptions |= View.SYSTEM_UI_FLAG_FULLSCREEN;
+                int newUiOptions = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        // Hide the nav bar and status bar
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN;
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
+                    newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE;
                 }
 
-                if (Build.VERSION.SDK_INT >= 16) {
-                    newUiOptions |= View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
-                }
-
-                if (Build.VERSION.SDK_INT >= 19) {
-                    newUiOptions |= View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                }
 
                 act.getWindow().getDecorView().setSystemUiVisibility(newUiOptions);
                 PopupLogUtil.trace("hideSystemBar");
