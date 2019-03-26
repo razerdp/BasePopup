eb8976168321d828ee2b0bcb5be37ff1459a994f
razerdp <razerdp123@gmail.com>
2019-03-26 16:25:25 +0800

    // smsCode优化

--- app/src/main/java/razerdp/demo/fragment/other/DialogPopupFrag.java
+++ app/src/main/java/razerdp/demo/fragment/other/DialogPopupFrag.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
 import razerdp.basepopup.BasePopupWindow;
 import razerdp.basepopup.R;
 import razerdp.demo.popup.DialogPopup;
