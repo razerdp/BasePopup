694dcec8681c77923c426d4b4e78107aed18053a
razerdp <razerdp123@gmail.com>
2020-05-17 15:42:14 +0800

    修复全屏Activity下SystemUiVisibility被强制修改的问题

---
+++ app/src/main/java/razerdp/demo/ui/fullscreen/FullScreenActivity.java
@@ -0,0 +1,55 @@
+package razerdp.demo.ui.fullscreen;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import butterknife.OnClick;
+import razerdp.basepopup.R;
+import razerdp.demo.base.baseactivity.BaseActivity;
+import razerdp.demo.popup.DemoPopup;
+
+/**
+ * Created by 大灯泡 on 2020/5/17.
+ */
+public class FullScreenActivity extends BaseActivity {
+    DemoPopup mDemoPopup;
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+
+    }
+
+    @Override
+    protected void onStartCreate(Bundle savedInstanceState) {
+        super.onStartCreate(savedInstanceState);
+//        requestWindowFeature(Window.FEATURE_NO_TITLE);
+//        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+    }
+
+
+    @Override
+    public int contentViewLayoutId() {
+        return R.layout.activity_full_screen;
+    }
+
+    @Override
+    protected void onInitView(View decorView) {
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @OnClick(R.id.tv_test)
+    void show() {
+        if (mDemoPopup == null) {
+            mDemoPopup = new DemoPopup(this);
+            mDemoPopup.setText("FullScreenTest");
+        }
+        mDemoPopup.showPopupWindow();
+    }
+}
