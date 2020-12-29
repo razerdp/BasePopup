8c5adc04ae5163f618ed073941aba3ffebe35aef
razerdp <razerdp123@gmail.com>
2020-12-29 18:58:52 +0800

    更新测试demo

---
+++ app/src/main/java/razerdp/demo/model/common/CommonUpdateInfo.java
@@ -0,0 +1,27 @@
+package razerdp.demo.model.common;
+
+import android.view.View;
+
+import razerdp.demo.model.DemoCommonUsageInfo;
+import razerdp.demo.ui.ActivityLauncher;
+import razerdp.demo.ui.updatetest.UpdateTestActivity;
+import razerdp.demo.utils.UIHelper;
+
+/**
+ * Created by 大灯泡 on 2020/12/29.
+ */
+public class CommonUpdateInfo extends DemoCommonUsageInfo {
+    public CommonUpdateInfo() {
+        title = "Update()方法测试";
+    }
+
+    @Override
+    public void toShow(View v) {
+        ActivityLauncher.start(v.getContext(), UpdateTestActivity.class);
+    }
+
+    @Override
+    public void toOption(View v) {
+        UIHelper.toast("这里啥都没有哦");
+    }
+}
